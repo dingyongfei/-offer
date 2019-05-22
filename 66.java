06b693e408ac064bd9d8ec84eb5e42153737f720
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-23 02:06:12 +0800

    Create 66.java

---
+++ 66.java
@@ -0,0 +1,24 @@
+public class Solution {
+	public int movingCount(int threshold, int rows, int cols) {
+		int flag[][] = new int[rows][cols];
+		return helper(0, 0, rows, cols, flag, threshold);
+	}
+	
+	private int helper(int i, int j, int rows, int cols, int[][] flag, int threshold) {
+		if (i < 0 || i > rows - 1 || j < 0 || j > cols - 1 || numSum(i) + numSum(j) > threshold || flag[i][j] == 1)
+			return 0;
+		flag[i][j] = 1;
+		return helper(i - 1, j, rows, cols, flag, threshold) + 
+			   helper(i + 1, j, rows, cols, flag, threshold) + 
+			   helper(i, j - 1, rows, cols, flag, threshold) + 
+			   helper(i, j + 1, rows, cols, flag, threshold) + 1;
+	}
+	
+	private int numSum(int i) {
+		int sum = 0;
+		do {
+			sum += i % 10;
+		} while ((i = i / 10) > 0);
+		return sum;
+	}
+}
