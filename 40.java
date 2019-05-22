e069d050ac6b2e33f83a95e7e5b2803f703d9f90
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-23 01:49:56 +0800

    Create 40.java

---
+++ 40.java
@@ -0,0 +1,30 @@
+public class Solution  {
+	public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+		if (array == null || array.length < 2) 
+			return;
+		int temp = 0;
+		for (int i = 0; i < array.length; i++) 
+			temp ^= array[i];
+		int indexOf1 = findFirstBitls(temp);
+		for (int i = 0; i < array.length; i++) {
+			if (isBit(array[i], indexOf1)) 
+				num1[0] ^= array[i];
+			else 
+				num2[0] ^= array[i];
+		}
+	}
+	
+	public int findFirstBitls(int num) {
+		int indexBit = 0;
+		while(((num & 1) == 0) && (indexBit) < 8 * 4) {
+			num = num >> 1;                     -----不能写成num >>= 1;
+			++indexBit;
+		}
+		return indexBit;
+	}
+	
+	public boolean isBit(int num, int indexBit) {
+		num = num >> indexBit;
+		return (num & 1) == 1;
+	}
+}
