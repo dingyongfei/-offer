dfd48496ab57cdb7716c767c474cfad81aa2d5a5
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-23 01:43:14 +0800

    Create 25.java

---
+++ 25.java
@@ -0,0 +1,73 @@
+public class Solution {
+
+	public static class Node {
+		public int value;
+		public Node next;
+		public Node rand;
+
+		public Node(int data) {
+			this.value = data;
+		}
+	}
+
+	public static Node copyListWithRand1(Node head) {
+		HashMap<Node, Node> map = new HashMap<Node, Node>();
+		Node cur = head;
+		while (cur != null) {
+			map.put(cur, new Node(cur.value));
+			cur = cur.next;
+		}
+		cur = head;                //注意  此处一定要归位。
+		while (cur != null) {              //注：不用判断cur.next，以将最后一个空节点设置好,一定要将最后一个节点设置为空！
+			map.get(cur).next = map.get(cur.next);
+			map.get(cur).rand = map.get(cur.rand);
+			cur = cur.next;
+		}
+		return map.get(head);
+	}
+
+	public static Node copyListWithRand2(Node head) {
+		if (head == null) {
+			return null;
+		}
+		Node cur = head;
+		Node next = null;
+		// copy node and link to every node
+		while (cur != null) {
+			next = cur.next;
+			cur.next = new Node(cur.value);
+			cur.next.next = next;
+			cur = next;
+		}
+		cur = head;
+		Node curCopy = null;
+		// set copy node rand
+		while (cur != null) {
+			next = cur.next.next;
+			curCopy = cur.next;
+			curCopy.rand = cur.rand != null ? cur.rand.next : null;
+			cur = next;
+		}
+
+       ---------------------------写法二--------------------------
+        //set copy node rand
+		
+        while (cur!=null) {
+             cur.next.random = cur.random == null ? null : cur.random.next;
+             cur = cur.next.next;
+        }
+        -----------------------------------------------------------
+
+		Node res = head.next;
+		cur = head;		// split
+		while (cur != null) {
+			next = cur.next.next;
+			curCopy = cur.next;
+			cur.next = next;
+			curCopy.next = next != null ? next.next : null;
+			cur = next;
+		}
+		return res;
+	}
+}
+
