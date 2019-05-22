89092b56498ca5beec89048cab2dc42c39d72dbe
dingyongfei <34892973+dingyongfei@users.noreply.github.com>
2019-05-23 01:44:32 +0800

    Create 27.java

---
+++ 27.java
@@ -0,0 +1,45 @@
+public class Solution {
+
+	public static void printAllPermutations1(String str) {
+		char[] chs = str.toCharArray();
+		process1(chs, 0);
+	}
+
+	public static void process1(char[] chs, int i) {
+		if (i == chs.length) {
+			System.out.println(String.valueOf(chs));
+// 注意 是String.valueOf(chs)方法，而不是chs.toString()!! 否则结果为hashCode()
+		}
+		for (int j = i; j < chs.length; j++) {
+			swap(chs, i, j);
+			process1(chs, i + 1);
+			swap(chs, i, j);
+		}
+	}
+
+	public static void printAllPermutations2(String str) {
+		char[] chs = str.toCharArray();
+		process2(chs, 0);
+	}
+
+	public static void process2(char[] chs, int i) {
+		if (i == chs.length) {
+			System.out.println(String.valueOf(chs));
+		}
+		HashSet<Character> set = new HashSet<>();
+		for (int j = i; j < chs.length; j++) {
+			if (!set.contains(chs[j])) {
+				set.add(chs[j]);
+				swap(chs, i, j);
+				process2(chs, i + 1);
+				swap(chs, i, j);
+			}
+		}
+	}
+
+	public static void swap(char[] chs, int i, int j) {
+		char tmp = chs[i];
+		chs[i] = chs[j];
+		chs[j] = tmp;
+	}
+}
